0fd181877ed54dbd38a3b7a3c1b16b1037fc97d7
Neressea <albert.vincent@hotmail.fr>
2015-05-06 19:06:49 +0200

    Beginning

---
+++ src/Movie.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Movie{
+
+	private String name, description, String director;
+	private int duration, year;
+	private ArrayList<String> type;
+	private ArrayList<String> actors;
+	private String type; //Movie or TV serie
+	private int id;
+
+	public static ArrayList<Movie> load(String file){
+
+	}
+}
