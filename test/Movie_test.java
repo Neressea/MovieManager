c392b1d0995e1500594792b15342fc61ff4e2181
Time-bomb <nicolas.bedrine@telecomnancy.net>
2015-05-24 17:10:35 +0200

    deb test

---
+++ test/Movie_test.java
@@ -0,0 +1,14 @@
+package test;
+
+import model;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Movie_test{
+
+	@Test	
+	public final void dist_test(){
+		Movie test = new Movie();
+	};
+}
