eb851463b886ae06ec55600498261b1b9fa09986
Time-bomb <nicolas.bedrine@telecomnancy.net>
2015-05-24 21:05:33 +0200

    pb test rélgé

---
+++ test/test/test_ListMovies.java
@@ -0,0 +1,17 @@
+package test;
+
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import model.*;
+
+public class test_ListMovies {
+	
+	@Test
+    public final void sort() {
+
+        fail("Not yet implemented"); // TODO
+
+    } 
+
+}
