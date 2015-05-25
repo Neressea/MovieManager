2c5513e2858add1652178ad6f505a995e0b99bef
Time-bomb <nicolas.bedrine@telecomnancy.net>
2015-05-25 12:24:47 +0200

    push

--- src/test/TestListMovies.java
+++ src/test/TestListMovies.java
@@ -13,13 +13,44 @@ public class TestListMovies {
 	
 	@Test
 	public final void test_findBarycentre(){
-		
+		Movie p = new Movie(0, "Film1", 0, 0, "Rea1", "TV", null , null, null);
+		Movie d = new Movie(0, "Film1", 0, 0, "Rea2", "Serie", null , null, null);
+		Movie e = new Movie(0, "Film2", 0, 0, "Rea2", "Serie", null , null, null);
+		Movie f = new Movie(0, "Film3", 0, 0, "Rea2", "Serie", null , null, null);
+		ListMovies<Movie> test = new ListMovies<Movie>(p);
+		test.addMovie(d);
+		test.addMovie(e);
+		test.addMovie(f);
+		test.findBarycentre();
+		assertTrue("List avec barycentre initialisée",test.getBarycentre() == d);
+		test = new ListMovies<Movie>();
+		test.addMovie(p);
+		test.addMovie(d);
+		test.addMovie(e);
+		test.addMovie(f);
+		test.findBarycentre();
+		assertTrue("List sans barycentre initialisée",test.getBarycentre() == d);
 	}
 	
 	@Test
     public final void test_sort() {
-
-        fail("Not yet implemented"); // TODO
+		Movie a = new Movie(0, "Film1", 0, 0, "Rea1", "TV", null , null, null);
+		Movie b = new Movie(0, "Film1", 0, 0, "Rea2", "Serie", null , null, null);
+		Movie c = new Movie(0, "Film2", 0, 0, "Rea2", "Serie", null , null, null);
+		Movie d = new Movie(0, "Film3", 0, 0, "Rea2", "Serie", null , null, null);
+		ListMovies<Movie> test = new ListMovies<Movie>(a);
+		test.addMovie(b);
+		test.addMovie(c);
+		test.addMovie(d);
+		assertTrue("List avec barycentre initialisée",test.sort(4,1,4).size() == 4);
+		test = new ListMovies<Movie>();
+		test.addMovie(a);
+		test.addMovie(b);
+		test.addMovie(c);
+		test.addMovie(d);
+		test.findBarycentre();
+		assertTrue("List sans barycentre initialisée",test.getBarycentre() == d);
+        
 
     } 
 	
@@ -34,57 +65,57 @@ public class TestListMovies {
 	}
 	
 	@Test
-	public final void toJson(){
-		
-	}
-	
-	@Test
-	public final void fromJson(){
-		
-	}
-	
-	@Test
-	public final void test_affiche_solve(){
-		
+	public final void test_toJson(){
+		Movie p = new Movie(0, "Film1", 0, 0, "Rea1", "TV", null , null, null);
+		Movie d = new Movie(0, "Film1", 0, 0, "Rea2", "Serie", null , null, null);
+		ListMovies<Movie> test = new ListMovies<Movie>(p);
+		test.addMovie(d);
+		String json = "{\"barycentre\":"+p.toJson()+",\"list\":["+d.toJson()+"]}";
+		assertTrue("List avec barycentre initialisée",test.toJson().equals(json));
+		test = new ListMovies<Movie>();
+		test.addMovie(p);
+		test.addMovie(d);
+		json = "{\"barycentre\":"+"\"null\""+",\"list\":["+p.toJson()+","+d.toJson()+"]}";
+		assertTrue("List sans barycentre initialisée",test.toJson().equals(json));
 	}
 	
 	@Test
 	public final void test_getBarycentre(){
-		ListMovies temp = new ListMovies();
+		ListMovies<Movie> temp = new ListMovies<Movie>();
 		Movie test = temp.getBarycentre(); 
-		assertTrue("Barycentre d'une ListMovies vide",test == null);
-		temp = new ListMovies(new Movie(0, "Film", 0, 0, "Rea1", null, null , null, null));
+		assertTrue("Barycentre d'une ListMovies<Movie> vide",test == null);
+		temp = new ListMovies<Movie>(new Movie(0, "Film", 0, 0, "Rea1", null, null , null, null));
 		test = temp.getBarycentre();
-		assertTrue("Barycentre d'une ListMovies initialisée à un film", test != null);
+		assertTrue("Barycentre d'une ListMovies<Movie> initialisée à un film", test != null);
 		temp.addMovie(new Movie(0, "Film", 0, 0, "Rea2", null, null , null, null));
 		temp.addMovie(new Movie(0, "Filmautre", 0, 0, "Rea1", null, null , null, null));
 		test = temp.getBarycentre();
-		assertTrue("Barycentre d'une ListMovies a plusieurs films", test != null);
+		assertTrue("Barycentre d'une ListMovies<Movie> a plusieurs films", test != null);
 	}
 	
 	@Test 
 	public final void test_getList(){
-		ListMovies temp = new ListMovies();
+		ListMovies<Movie> temp = new ListMovies<Movie>();
 		ArrayList<Movie> test = temp.getList(); 
-		assertTrue("Liste d'une ListMovies vide",test.isEmpty());
-		temp = new ListMovies(new Movie(0, "Film", 0, 0, "Rea1", null, null , null, null));
+		assertTrue("Liste d'une ListMovies<Movie> vide",test.isEmpty());
+		temp = new ListMovies<Movie>(new Movie(0, "Film", 0, 0, "Rea1", null, null , null, null));
 		test = temp.getList();
-		assertTrue("Liste d'une ListMovies initialisée à un film", test.isEmpty());
+		assertTrue("Liste d'une ListMovies<Movie> initialisée à un film", test.isEmpty());
 		temp.addMovie(new Movie(0, "Film", 0, 0, "Rea2", null, null , null, null));
 		temp.addMovie(new Movie(0, "Filmautre", 0, 0, "Rea1", null, null , null, null));
 		test = temp.getList();
-		assertTrue("Liste d'une ListMovies a plusieurs films", !test.isEmpty());
+		assertTrue("Liste d'une ListMovies<Movie> a plusieurs films", !test.isEmpty());
 	}
 	
 	@Test
 	public final void test_addMovie(){
-		ListMovies temp = new ListMovies();
+		ListMovies<Movie> temp = new ListMovies<Movie>();
 		assertTrue("Liste vide sans barycentre",temp.getList().size() == 0 && temp.getBarycentre() == null);
 		temp.addMovie(new Movie(0, "Film", 0, 0, "Rea2", null, null , null, null) );
 		assertTrue("Ajout d'un film sans barycentre",temp.getList().size() == 1 && temp.getBarycentre() == null);
 		temp.addMovie(new Movie(0, "Filmautre", 0, 0, "Rea1", null, null , null, null));
 		assertTrue("Ajout de deux films sans barycentre",temp.getList().size() == 2 && temp.getBarycentre() == null);
-		temp = new ListMovies(new Movie(0, "Film", 0, 0, "Rea1", null, null , null, null));
+		temp = new ListMovies<Movie>(new Movie(0, "Film", 0, 0, "Rea1", null, null , null, null));
 		assertTrue("Liste vide avec barycentre",temp.getList().size() == 0 && temp.getBarycentre() != null);
 		temp.addMovie(new Movie(0, "Film", 0, 0, "Rea2", null, null , null, null) );
 		assertTrue("Ajout d'un film avec barycentre",temp.getList().size() == 1 && temp.getBarycentre() != null);
@@ -95,25 +126,25 @@ public class TestListMovies {
 	
 	@Test
 	public final void test_getRowCount(){
-		ListMovies temp = new ListMovies();
-		assertTrue("Taille d'une ListMovies vide",temp.getList().size() == temp.getRowCount() && temp.getBarycentre() == null);
+		ListMovies<Movie> temp = new ListMovies<Movie>();
+		assertTrue("Taille d'une ListMovies<Movie> vide",temp.getList().size() == temp.getRowCount() && temp.getBarycentre() == null);
 		temp.addMovie(new Movie(0, "Film", 0, 0, "Rea2", null, null , null, null) );
-		assertTrue("Taille d'une ListMovies de 1 film",temp.getList().size() == temp.getRowCount() && temp.getBarycentre() == null);
+		assertTrue("Taille d'une ListMovies<Movie> de 1 film",temp.getList().size() == temp.getRowCount() && temp.getBarycentre() == null);
 		temp.addMovie(new Movie(0, "Filmautre", 0, 0, "Rea1", null, null , null, null));
-		assertTrue("Taille d'une ListMovies de 2 films",temp.getList().size() == temp.getRowCount() && temp.getBarycentre() == null);
+		assertTrue("Taille d'une ListMovies<Movie> de 2 films",temp.getList().size() == temp.getRowCount() && temp.getBarycentre() == null);
 	}
 	
 	@Test
 	public final void test_getColumnCount(){
-		ListMovies temp = new ListMovies();
+		ListMovies<Movie> temp = new ListMovies<Movie>();
 		assertTrue("Taille de head de liste sans barycentre",temp.getColumnCount() == 9);
-		temp = new ListMovies(new Movie());
+		temp = new ListMovies<Movie>(new Movie());
 		assertTrue("Taille de head de liste avec barycentre",temp.getColumnCount() == 9);
 	}
 	
 	@Test
 	public final void test_getColumnName(){
-		ListMovies temp = new ListMovies();
+		ListMovies<Movie> temp = new ListMovies<Movie>();
 		assertTrue("Obtenir Id",temp.getColumnName(0) == "Id");
 		assertTrue("Obtenir Titre",temp.getColumnName(1) == "Titre");
 		assertTrue("Obtenir Année",temp.getColumnName(2) == "Année");
@@ -130,7 +161,7 @@ public class TestListMovies {
 	public final void test_trier_dist_barycentre(){
 		ArrayList<String> genre= new ArrayList<String>();
 		genre.add("Genre1");
-		ListMovies temp = new ListMovies(new Movie(0, "Film", 0, 0, "Rea2", "test", null , genre, null));
+		ListMovies<Movie> temp = new ListMovies<Movie>(new Movie(0, "Film", 0, 0, "Rea2", "test", null , genre, null));
 		Movie test = new Movie(1, "Film", 0, 0, "Rea1", "test", null , genre, null);
 		genre.remove("Genre1");
 		genre.add("Genre2");
